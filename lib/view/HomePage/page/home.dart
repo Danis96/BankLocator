ccfb2e37feb90d2547d35276d6814788ade7a639
Danis96 <danis.preldzic@gmail.com>
2020-08-26 08:43:17 +0200

    cardDesign-ListScreen

--- lib/view/HomePage/page/home.dart
+++ lib/view/HomePage/page/home.dart
@@ -1,9 +1,11 @@
 import 'package:banklocator/utils/colors.dart';
 import 'package:banklocator/utils/size_config.dart';
+import 'package:banklocator/view-model/home-view-model.dart';
 import 'package:banklocator/view/HomePage/widgets/bankCard.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+
   final secondaryColor = AppColors().secondaryColor;
   final dominantColor = AppColors().dominantColor;
   final btnColor = AppColors().btnGreen;
@@ -12,88 +14,64 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Container(
-      child: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            tileMode: TileMode.clamp,
-            colors: [secondaryColor, dominantColor],
-          ),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          tileMode: TileMode.clamp,
+          colors: [secondaryColor, dominantColor],
         ),
-        child: Scaffold(
-          bottomNavigationBar: Row(
-             mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Container(
-                          height: SizeConfig.blockSizeHorizontal * 11,
-                          width: SizeConfig.blockSizeHorizontal * 33.3,
-                          child: RaisedButton(
-                            color: btnColor,
-                            onPressed: () => {},
-                            child: Text(
-                              'My location',
-                              style: TextStyle(
-                                  fontSize: SizeConfig.safeBlockHorizontal * 4),
-                            ),
-                          )),
-                      Container(
-                        height: SizeConfig.blockSizeHorizontal * 11,
-                        width: SizeConfig.blockSizeHorizontal * 33.3,
-                        child: RaisedButton(
-                          color: btnColor,
-                          onPressed: () => {},
-                          child: Text(
-                            'Location',
-                            style: TextStyle(
-                                fontSize: SizeConfig.safeBlockHorizontal * 4),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: SizeConfig.blockSizeHorizontal * 11,
-                        width: SizeConfig.blockSizeHorizontal * 33.3,
-                        child: RaisedButton(
-                          color: btnColor,
-                          onPressed: () => {},
-                          child: Text(
-                            'List',
-                            style: TextStyle(
-                                fontSize: SizeConfig.safeBlockHorizontal * 4),
-                          ),
-                        ),
-                      )
-                    ],
-          ),
-            backgroundColor: Colors.transparent,
-            appBar: AppBar(
-              flexibleSpace: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    tileMode: TileMode.clamp,
-                    colors: [secondaryColor, dominantColor],
+      ),
+      child: Scaffold(
+        bottomNavigationBar: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+                height: SizeConfig.blockSizeHorizontal * 11,
+                width: SizeConfig.blockSizeHorizontal * 33.3,
+                child: RaisedButton(
+                  color: btnColor,
+                  onPressed: () => {},
+                  child: Text(
+                    'My location',
+                    style: TextStyle(
+                        fontSize: SizeConfig.safeBlockHorizontal * 4),
                   ),
+                )),
+            Container(
+              height: SizeConfig.blockSizeHorizontal * 11,
+              width: SizeConfig.blockSizeHorizontal * 33.3,
+              child: RaisedButton(
+                color: btnColor,
+                onPressed: () => {},
+                child: Text(
+                  'Location',
+                  style: TextStyle(
+                      fontSize: SizeConfig.safeBlockHorizontal * 4),
                 ),
               ),
-              title: Text('Mobile Assignment'),
-              centerTitle: true,
             ),
-            body: ListView(
-              shrinkWrap: true,
-              children: [
-                Container(
-                  child: ListView.builder(
-                      
-                      shrinkWrap: true,
-                      itemCount: 8,
-                      itemBuilder: (BuildContext context, int number) {
-                        return bankCard(context);
-                      }),
+            Container(
+              height: SizeConfig.blockSizeHorizontal * 11,
+              width: SizeConfig.blockSizeHorizontal * 33.3,
+              child: RaisedButton(
+                color: btnColor,
+                onPressed: () => HomeViewModel().navigateToListOfBanks(context),
+                child: Text(
+                  'List',
+                  style: TextStyle(
+                      fontSize: SizeConfig.safeBlockHorizontal * 4),
                 ),
-              ],
-            )),
+              ),
+            )
+          ],
+        ),
+        backgroundColor: Colors.transparent,
+        body: Container(
+          child: Center(
+            child: Text('Ovdje ide mapa'),
+          ),
+        ),
       ),
     );
   }
